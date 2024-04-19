887bb566415a6205b2d333135ddaff6752daef9b
namkha1032 <namkha1032@gmail.com>
2024-04-19 09:13:01 +0700

    remove mysql, fix .gitignore

--- be/app.js
+++ be/app.js
@@ -6,10 +6,10 @@
 const express = require('express')
 const app = express()
 // Include routers
-const notesRouter = require('./controllers/notes')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
-const roomRouter = require('./controllers/room')
+// const notesRouter = require('./controllers/notes')
+// const usersRouter = require('./controllers/users')
+// const loginRouter = require('./controllers/login')
+// const roomRouter = require('./controllers/room')
 const hotelRouter = require('./controllers/hotel')
 // CORS
 const cors = require('cors')
@@ -22,10 +22,10 @@ const middleware = require('./utils/middleware')
 
 app.use(cors())
 app.use(express.json())
-app.use('/api/notes', notesRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
-app.use('/api/room', roomRouter)
+// app.use('/api/notes', notesRouter)
+// app.use('/api/users', usersRouter)
+// app.use('/api/login', loginRouter)
+// app.use('/api/room', roomRouter)
 app.use('/api/hotel', hotelRouter)
 //////////////////////////////////////////////////////////////////////////
 
